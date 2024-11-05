d37dde8545abd8257fed00c94e1e0f63b6970ac7
Matias <matias.gallo@paygoal.com.ar>
2024-11-05 11:12:28 -0300

    refactor

--- src/test/java/Dto/CountryDto.java
+++ src/test/java/Dto/CountryDto.java
@@ -7,7 +7,6 @@ public class CountryDto {
     public String capital;
     public String flagDescription;
     public String domain;
-    public Integer population;
 
     public void setFlagDescription(String flagDescription) {
         this.flagDescription = flagDescription.toLowerCase();
