de757450c03783089e423c713debdb6a5722cf79
Matias <matias.gallo@paygoal.com.ar>
2024-11-08 18:15:15 -0300

    refactor

--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -62,41 +62,4 @@ public class Utils {
         return gson.toJson(perceptionRequestDto);
     }
 
-    public String mapExcelToJsonRequest(List<Map<String, String>> data, int row) {
-        PaymentRequestDto perceptionRequestDto = new PaymentRequestDto();
-        perceptionRequestDto.setDate(LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()));
-        perceptionRequestDto.setExternalId(UUID.randomUUID().toString());
-
-        TaxpayerDto taxpayerDto = new TaxpayerDto();
-        taxpayerDto.setTaxpayerId(Long.valueOf(data.get(row).get("taxpayerId")));
-        taxpayerDto.setJurisdiction(Long.valueOf(data.get(0).get("jurisdiction")));
-        perceptionRequestDto.setTaxpayer(taxpayerDto);
-
-        TransactionDto transactionDto = new TransactionDto();
-        transactionDto.setAmount(new BigDecimal(data.get(row).get("amount")).setScale(2, RoundingMode.HALF_UP));
-        transactionDto.setPaymentMethod(data.get(row).get("paymentMethod"));
-        transactionDto.setCurrency(data.get(row).get("currency"));
-        perceptionRequestDto.setTransaction(transactionDto);
-
-        MerchantDto newMerchant = new MerchantDto();
-        newMerchant.setSoftDescriptor(data.get(row).get("merchant"));
-        perceptionRequestDto.setMerchant(newMerchant);
-
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(LocalDateTime.class, new JsonSerializer<LocalDateTime>() {
-                    @Override
-                    public JsonElement serialize(LocalDateTime src, Type typeOfSrc, JsonSerializationContext context) {
-                        return new JsonPrimitive(src.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-                    }
-                })
-                .create();
-        return gson.toJson(perceptionRequestDto);
-
-    }
-
-    @SneakyThrows
-    public String getContentFromFile(String filePath) {
-        return Files.readString(Paths.get(filePath));
-    }
-
 }
